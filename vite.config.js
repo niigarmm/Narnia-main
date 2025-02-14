d1f5818c372051509da92b1d74f4bd416e20b84e
niigarmm <nigar03mahmudova@gmail.com>
2025-02-15 02:45:03 +0400

    add

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from "dotenv";
+dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    target: "esnext" // Modern tarayıcıları destekler
-  }
+ 
 })
